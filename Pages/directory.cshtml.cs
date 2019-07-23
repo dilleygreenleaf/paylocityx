03d6a529f75e3fc8640a0139c929b906613b3f2d
“dilleygreenleaf” <rjackson@icstars.org>
2019-07-23 17:31:03 -0500

    Add project files.

---
+++ Pages/directory.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace paylocityx.Pages
+{
+    public class directoryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
